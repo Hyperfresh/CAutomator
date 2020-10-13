666f048baa3d0e0332e98044cbfb1a21b415807d
Hy~ <31476608+Hyperfresh@users.noreply.github.com>
2020-10-13 11:42:18 +1030

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,23 @@
+/*
+       _
+  /\__| |__/\ 
+  \   ___   / __                 _                                     _
+ _/  /   \  \/  \              _| |_                                 _| |_
+|_  |     \ /    \     _   _  |_   _|  _____   _________   _____ _  |_   _|  _____   _____
+ _\  \_____/  /\  \   | | | |   | |   |  _  | |  _   _  | |  _  | |   | |   |  _  | |  ___|
+ \________/  /__\  \  | |_| |   | |   | |_| | | | | | | | | |_|   |   | |   | |_| | | |
+         /__/    \__\ |_____|   |_|   |_____| |_| |_| |_| |_____|_|   |_|   |_____| |_|
+         
+CAutomator - the custom-built Discord bot, coded in node.js
+Copyright (C) 2020 Hyperfresh | https://github.com/Hyperfresh/CAutomator/
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Lesser General Public License for more details.
+*/
