ca7026ddfcac5a31317b512a43cc21d4a0f24a5e
Hy~ <31476608+Hyperfresh8080@users.noreply.github.com>
2021-03-08 13:08:12 +1030

    Create role.js

---
+++ modules/speciality/role.js
@@ -0,0 +1,46 @@
+// Taken from the Custom Role system in CAutomator.py and enhanced a bit.
+// Thanks to http://github.com/iwa for the original code!
+
+// Importing prefix
+const config = require('../../data/config.json');
+let prefix = config.PREFIX
+
+// Set up database based on lowdb.
+const lowdb = require("lowdb");
+const FileSync = require('lowdb/adapters/FileSync')
+const adapter = new FileSync(`${config.DIR}/data/database.json`)
+const db = lowdb(adapter)
+db.defaults({ roles: [], members: [], count: 0 })
+        .write()
+
+module.exports = {
+    commands: 'role',
+    minArgs: 0,
+    maxArgs: 500,
+    callback: (message, args) => {
+        let rolename = "";
+        let search = db.get('roles')
+            .find({memberid: message.author.id})
+            .value()
+        if (!search) {
+            for (let x = 0; x > ((args.length)-1); x++) {
+                rolename = `${rolename} ${args[x]}`
+            }
+            if (/^#(?:[0-9a-fA-F]{3}){1,2}$/.test(args[((args.length)-1)])) {
+                let rolecolour = parseInt(args[((args.length)-1)], 16)
+                let role = guild.roles.create({
+                    data: {
+                      name: rolename,
+                      color: rolecolour,
+                      hoist: true,
+                    },
+                  })
+                    .then(console.log)
+                    .catch(console.error);
+                db.get('roles')
+                  .push({memberid: message.author.id, roleid: role.id})
+                  .write()
+            }
+        }
+    }
+}
