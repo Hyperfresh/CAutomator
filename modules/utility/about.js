6c3b4fe9a1e2448e0a50afbd05de3a05b6e3c516
Hy~ <31476608+Hyperfresh8080@users.noreply.github.com>
2021-03-14 21:55:35 +1030

    update about command

--- modules/utility/about.js
+++ modules/utility/about.js
@@ -1,22 +1,33 @@
 // Importing discord.js for handling embeds
 const Discord = require('discord.js')
+const client = require('../../index').client
 
 // Get info on bot details
 const botinfo = require('../../package.json')
 const process = require('process')
 const os = require('os')
 
+const {DateTime} = require('luxon')
+const luxonver = require('luxon').VERSION
+const timez = require('moment-timezone')
+
 module.exports = {
     commands: 'about',
     minArgs: 0,
     maxArgs: 0,
     callback: (message, text) => {
+        let colour = message.guild.member(client.user.id)
+
         let embed = new Discord.MessageEmbed()
-            .setAuthor('About CAutomator','https://github.com/Hyperfresh/CAutomator/blob/master/resources/icon.png?raw=true')
+            .setAuthor('About CAutomator')
             .setTitle(`CAutomator v${botinfo.version}`)
-            .setDescription('CAutomator is a custom-built bot for this server. [Learn more here!](http://github.com/hyperfresh/CAutomator)')
+            .setColor(colour.roles.highest.color)
+            .setURL(`https://github.com/Hyperfresh/CAutomator/releases/tag/v${botinfo.version}`)
+            .setThumbnail(`https://cdn.discordapp.com/avatars/${client.user.id}/${client.user.avatar}.png?size=512`)
+            .setDescription('<:cautomator:798916996401594368> **CAutomator** is a custom-built Discord bot, built in <:nodejs:817518471226720256> node.js for the <:CalculatedAnarchy:584304539717599234> Calculated Anarchy server.\n[Learn more about me here!](http://github.com/hyperfresh/CAutomator)')
             .addField('OS & Node Installation',`**OS**: ${os.version()}\n**Node**: ${process.version}`,true)
-            .addField('Main dependencies',`**discord.js**: ${Discord.version}\n**lowdb**: ${botinfo.dependencies.lowdb}\n**shell.js**: ${botinfo.dependencies.shelljs}`,true)
+            .addField('Main dependencies',`**discord.js**: ${Discord.version}\n**lowdb**: ${botinfo.dependencies.lowdb}\n**shell.js**: ${botinfo.dependencies.shelljs}\n**luxon**: ${luxonver}\n**moment-timezone**: ${timez.version}`,true)
+            .addField('Server time',DateTime.now().toLocaleString(DateTime.DATETIME_HUGE))
             .setFooter('Created by Hyperfresh#8080','https://media.discordapp.net/attachments/634575479042474003/663591393754742794/emote.gif')
         message.channel.send(embed)
     },
