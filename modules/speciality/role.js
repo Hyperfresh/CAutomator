c83628605f942ebaca611e6fd9bddf07e6f94b4f
Hy~ <31476608+Hyperfresh8080@users.noreply.github.com>
2021-03-14 10:31:54 +1030

    update role command make it so people can edit and remove role

--- modules/speciality/role.js
+++ modules/speciality/role.js
@@ -10,13 +10,14 @@ const lowdb = require("lowdb");
 const FileSync = require('lowdb/adapters/FileSync')
 const adapter = new FileSync(`${config.DIR}/data/database.json`)
 const db = lowdb(adapter)
-db.defaults({ roles: [], members: [], count: 0 })
+db.defaults({ roles: [], rcount: 0, profiles: [], pcount: 0 })
         .write()
 
 module.exports = {
     commands: 'role',
     minArgs: 0,
     maxArgs: 500,
+    requiredRoles: ['Level 30+'],
     callback: (message, args) => {
         let rolename = "";
         let search = db.get('roles')
@@ -28,11 +29,13 @@ module.exports = {
             }
             if (/^(?:[0-9a-fA-F]{3}){1,2}$/.test(args[0])) {
                 let rolecolour = parseInt(args[0], 16)
-                let role = message.guild.roles.create({
+                let pos = db.get('rcount').value()
+                message.guild.roles.create({
                     data: {
                       name: rolename,
                       color: rolecolour,
-                      hoist: true
+                      hoist: true,
+                      position: message.guild.roles.cache.size-pos
                     },
                   })
                     .then(role => {
@@ -40,9 +43,38 @@ module.exports = {
                             .push({memberid: message.author.id, roleid: role.id})
                             .write()
                         message.member.roles.add(role.id)
-                        message.reply(`Role given: <@&${role.id}>`)
+                        db.update('rcount', n => n + 1)
+                            .write()
+                        message.reply(`I've created your custom role: <@&${role.id}>`)
                     })
                     .catch(console.error);
+            } else {
+                message.reply('you didn\'t specify a colour (or didn\'t specify it properly)! Do in the following format: `~role <colour> <role name>`')
+            }
+        } else {
+            if (args[0] == "remove") {
+                message.member.roles.remove(search.roleid)
+                    .then(message.reply('I\'ve removed your custom role.'))
+                return
+            }
+            for (let x = 1; x < (args.length); x++) {
+                rolename = `${rolename} ${args[x]}`
+            }
+            if (/^(?:[0-9a-fA-F]{3}){1,2}$/.test(args[0])) {
+                let rolecolour = parseInt(args[0], 16)
+                message.guild.roles.fetch(search.roleid)
+                    .then(role => {
+                        role.edit({
+                            name: rolename,
+                            color: rolecolour
+                        })
+                        message.reply(`I've edited your custom role: <@&${role.id}>`)
+                        try {
+                            message.member.roles.add(role.id)
+                        } catch { console.log('Member may already have role') }
+                    })
+            } else {
+                message.reply('you didn\'t specify a colour (or didn\'t specify it properly)! Do in the following format: `~role <colour> <role name>`')
             }
         }
     }
