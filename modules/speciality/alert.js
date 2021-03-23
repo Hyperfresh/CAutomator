fb2001967cdd45fb801974272bee82b5266d2f49
Hy~ <31476608+Hyperfresh8080@users.noreply.github.com>
2021-03-23 13:54:59 +1030

    Update alert.js

--- modules/speciality/alert.js
+++ modules/speciality/alert.js
@@ -3,13 +3,39 @@ const parser = new Parser();
 const xml2js = require('xml2js')
 const xmlparser = new xml2js.Parser();
 
+const Discord = require('discord.js')
+
+const { DateTime } = require('luxon')
+
+function createAlertEmbed(result,num) {
+    let alert = result.div.alert[num].info[0]
+    let embed = Discord.MessageEmbed()
+        .setAuthor(alert.parameter[2].value[0].toUpperCase())
+    if (alert.parameter[2].value[0] == 'Bushfire Advice') {
+        embed.setThumbnail('https://github.com/Hyperfresh/CAutomator/blob/dev/resources/alert-symbols/ico-fire-yellow.png?raw=true')
+        embed.setColor(16506930)
+    } else if (alert.parameter[2].value[0] == 'Bushfire Watch and Act') {
+        embed.setThumbnail('https://github.com/Hyperfresh/CAutomator/blob/dev/resources/alert-symbols/ico-fire-orange.png?raw=true')
+        embed.setColor(16742656)
+    } else if (alert.parameter[2].value[0] == 'Bushfire Emergency Warning') {
+        embed.setThumbnail('https://github.com/Hyperfresh/CAutomator/blob/dev/resources/alert-symbols/ico-fire-red.png?raw=true')
+        embed.setColor(14024732)
+    }
+    embed.setTitle(alert.area[0].areaDesc[0])
+
+    let issued = DateTime.fromISO(alert.effective[0]).toLocaleString(DateTime.DATETIME_FULL)
+    let expire
+    embed.setDescription(`> **Issued ${issued}**\n${alert.description[0]}`)
+
+    embed.addField('What you should do',alert.instruction[0])
+}
 
 module.exports = {
     commands: 'alert',
     minArgs: 0,
     maxArgs: 0,
     callback: (message) => {
-        let RSS_URL = 'http://127.0.0.1:5500/test.xml' // 'https://data.eso.sa.gov.au/prod/cfs/criimson/cfs_current_incidents.xml' // 'http://data.eso.sa.gov.au/prod/cfs/criimson/cfs_cap_incidents.xml'
+        let RSS_URL = 'https://hyperfresh.github.io/test.xml' // 'https://data.eso.sa.gov.au/prod/cfs/criimson/cfs_current_incidents.xml' // 'http://data.eso.sa.gov.au/prod/cfs/criimson/cfs_cap_incidents.xml'
         parser.parseURL(RSS_URL)
             .then(res => {
                 console.log(res.items[0].content)
