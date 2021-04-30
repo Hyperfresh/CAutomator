3cc62a6e66b88cb82675a969c2fbf2ed8813e97a
Hy~ <hyperfresh@outlook.com.au>
2021-04-30 22:08:23 +0930

    Create break.js
    Why not have a bit of fun? Done as per #49.

---
+++ modules/fun/break.js
@@ -0,0 +1,16 @@
+let breakMessages = [
+    'oh no you have broken me!',
+    'Oh no, hairpins busted',
+    "'JHsfhishi28778878v...'''s''df'4'4'433'101011011001'",
+    "oh no, now my brain has turned into mash potat"
+]
+
+module.exports = {
+    commands: 'break',
+    minArgs: 0,
+    maxArgs: 0,
+    callback: (message) => {
+        let item = Math.random() * 3
+        message.channel.send(`${breakMessages[item.toFixed(0)]}`)
+    }
+}
